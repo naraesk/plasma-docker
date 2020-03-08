7fcde4d18e1c56253facce9e0d1edb307c57c251
Benjamin Quinn <benjamin.quinn@vroom.com>
2020-03-08 14:58:54 -0400

    Set exit on failure flag in install.sh
    Fixes issue in install where commands continue running after fatal
    errors occur.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir build -p && cd build
 cmake -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON ../
 make
